73a1a29600a81374ca49b3781829b170c7a9a12d
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-04-28 17:29:45 +0300

    logout page for user

--- feedback_project/client/app/board/page.tsx
+++ feedback_project/client/app/board/page.tsx
@@ -1,4 +1,7 @@
 'use client'
+import Cardone from '@/components/board/cards/cardone'
+import Cardtwo from '@/components/board/cards/cardtwo'
+import TableOne from '@/components/board/tables/tableone'
 import { Card, CardBody, Divider, Text } from '@chakra-ui/react'
 import React from 'react'
 
@@ -9,22 +12,14 @@ export default function page() {
     <div>
       <div className='ml-5 mt-4 mr-5'>
         <p className='text-xl'>Burda geri bildirimleriniz görünür.</p>
-        <Divider />
+        <Divider className='mb-4' />
 
-        <div className="grid grid-cols-2 gap-1">
+        <Cardone />
+    
+        <div className="grid grid-cols-2 gap-1 mt-3">
           <div className='bg-gray-400'>
             Burada Geri Bildirimler
-            <p>fdasfsdfsdfsdfsdgfsdfsd</p>
-            <p>fdasfsdfsdfsdfsdgfsdfsd</p>
-            <p>fdasfsdfsdfsdfsdgfsdfsd</p>
-            <p>fdasfsdfsdfsdfsdgfsdfsd</p>
-            <p>fdasfsdfsdfsdfsdgfsdfsd</p>
-            <p>fdasfsdfsdfsdfsdgfsdfsd</p>
-            <p>fdasfsdfsdfsdfsdgfsdfsd</p>
-            <p>fdasfsdfsdfsdfsdgfsdfsd</p>
-            <p>fdasfsdfsdfsdfsdgfsdfsd</p>
-            <p>fdasfsdfsdfsdfsdgfsdfsd</p>
-            <p>fdasfsdfsdfsdfsdgfsdfsd</p>
+
 
           </div>
           <div className='bg-gray-400'>
--- feedback_project/client/app/board/settings/page.tsx
+++ feedback_project/client/app/board/settings/page.tsx
@@ -1,11 +1,43 @@
+'use client'
+
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Heading, Link, SimpleGrid, Text } from '@chakra-ui/react'
 import React from 'react'
 
-type Props = {}
-
-export default function page({ }: Props) {
+export default function page() {
   return (
     <div className='ml-5 mt-9'>
-      Settings
+      <h1 className='text-xl'>Ayarlar</h1>
+      <Divider className='mb-3' />
+      <SimpleGrid spacing={4} templateColumns='repeat(auto-fill, minmax(600px, 1fr))' className='mr-4'>
+        <Card bgColor="blackAlpha.300" textColor="white">
+          <CardHeader>
+            <Heading size='md'> Kullanıcı Bilgileri</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text className='mb-2 underline'>İsim: </Text>
+            <Text className='mb-2 underline'>Email: </Text>
+            <Text className='mb-2 underline'>Adress: </Text>
+            <Text className='mb-2 underline'>Şehir: </Text>
+            <Text className='underline'>İlçe: </Text>
+
+          </CardBody>
+          <CardFooter>
+            <Link href='https://chakra-ui.com' isExternal>
+              Bilgileri Düzenle <ExternalLinkIcon mx='2px' />
+            </Link>
+          </CardFooter>
+        </Card>
+        <Card bgColor="blackAlpha.300" textColor="white">
+          <CardHeader>
+            <Heading size='md'> Kullanıcı İşlemleri</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text className='hover:underline hover:cursor-pointer'>Parola Değiştir</Text>
+          </CardBody>
+        </Card>
+
+      </SimpleGrid>
     </div>
   )
 }
