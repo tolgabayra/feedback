db3e77ec6e541d063bf34832221ec4807b5a57f3
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-05-04 21:48:39 +0300

    default page is okey

--- feedback_project/client/app/page.tsx
+++ feedback_project/client/app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-   <>Deneme</>
+    <>
+      <p className="text-center text-3xl mt-1">Hoşgeldiniz</p>
+      <div className="text-center mt-5 hover:underline hover:text-blue-600 duration-300">
+        <Link href="/signin">Giriş Sayfası</Link>
+      </div>
+    </>
   )
 }
