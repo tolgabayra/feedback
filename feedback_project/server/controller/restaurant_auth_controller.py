be6e6bc4e5d2e8f25e5d17e725806f5f69ca34fe
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-05-05 16:45:34 +0300

    added jwt verify and refresh token

--- feedback_project/server/controller/restaurant_auth_controller.py
+++ feedback_project/server/controller/restaurant_auth_controller.py
@@ -39,9 +39,41 @@ def register():
     restaurant = RestaurantAuthService.register(name, address, email, password, district_id, city_id)
     return jsonify({"Message": "Your restaurant account created is succesfull"}), 201
 
+
 @restaurant_auth_controller.route("/logout", methods=["POST"])
 def logout():
     response = jsonify({"Message": "You are logged out"})
     response.delete_cookie("access_token")
     response.delete_cookie("refresh_token")
+    return response, 200
+
+
+
+@restaurant_auth_controller.route("/verify", methods=["POST"])
+@jwt_required
+def get_information():
+    return jsonify({"Message": "Okey"}), 200
+    
+
+
+@restaurant_auth_controller.route("/refresh_token", methods=["POST"])
+def refresh_token():
+    refresh_token = request.cookies.get('refresh_token')
+
+    if not refresh_token:
+        return jsonify({"message": "Refresh token not found"}), 400
+    
+    access_token = request.cookies.get('access_token')
+
+    decoded_token = Helper.decode_token(access_token)
+    print(decoded_token)
+
+    new_access_token = Helper.generate_access_token(decoded_token)
+
+    if not new_access_token:
+        return jsonify({"message": "Invalid refresh token"}), 400
+
+    response = jsonify({"access_token": new_access_token})
+    response.set_cookie('access_token', new_access_token, httponly=True)
+
     return response, 200
