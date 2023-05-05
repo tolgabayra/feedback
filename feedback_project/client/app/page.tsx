cfdbb9f3898bb5dbde6a528fd7661ce98d9b8ca1
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-05-05 15:06:24 +0300

    mantine ui added and root layout provider is okey

--- feedback_project/client/app/page.tsx
+++ feedback_project/client/app/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { Button } from "@mantine/core";
 import Link from "next/link";
 
 
@@ -6,6 +8,7 @@ export default function Home() {
     <>
       <p className="text-center text-3xl mt-1">Hoşgeldiniz</p>
       <div className="text-center mt-5 hover:underline hover:text-blue-600 duration-300">
+        <Button>Deneme</Button>
         <Link href="/signin">Giriş Sayfası</Link>
       </div>
     </>
