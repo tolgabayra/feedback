9d4dca72653ec4584d331a881c1ca722f26af2c0
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-05-13 18:38:32 +0300

    auth loading and localStorage setExpand value checking added

--- feedback_project/client/app/(auth)/signin/page.tsx
+++ feedback_project/client/app/(auth)/signin/page.tsx
@@ -1,10 +1,12 @@
 'use client'
 import { notifications } from '@mantine/notifications';
-import Link from 'next/link'
+import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import { LoadingOverlay } from '@mantine/core';
 
 export default function SignIn() {
+  const [loading, setLoading] = useState(false)
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const router = useRouter();
@@ -27,6 +29,7 @@ export default function SignIn() {
           color: 'green',
           autoClose: 1500
         })
+        setLoading(true)
         setTimeout(() => {
           router.push("/dashboard")
         }, 1500)
@@ -59,50 +62,58 @@ export default function SignIn() {
 
   return (
     <section className="bg-gradient-to-b from-gray-100 to-white">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-16 md:pb-20">
+      {
+        loading ? <LoadingOverlay
+          loaderProps={{ size: 'sm', color: 'blue', variant: 'bars' }}
+          overlayOpacity={0.3}
+          overlayColor="#c5c5c5"
+          visible
+        /> :
+          <div className="max-w-6xl mx-auto px-4 sm:px-6">
+            <div className="pt-32 pb-12 md:pt-16 md:pb-20">
 
-          {/* Page header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <h1 className="h1">Hoşgeldiniz</h1>
-          </div>
-          {/* Form */}
-          <div className="max-w-sm mx-auto">
-            <form onSubmit={handleLogin}>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email</label>
-                  <input onChange={(e) => setEmail(e.target.value)} id="email" type="email" className="form-input w-full text-gray-800" placeholder="Email adresinizi giriniz" required />
-                </div>
+              {/* Page header */}
+              <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+                <h1 className="h1">Hoşgeldiniz</h1>
               </div>
-              <div className="flex flex-wrap -mx-3 mb-4">
-                <div className="w-full px-3">
-                  <div className="flex justify-between">
-                    <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="password">Parola</label>
-                    <Link href="/reset-password" className="text-sm font-medium text-blue-600 hover:underline">Parolamı Unuttum ?</Link>
+              {/* Form */}
+              <div className="max-w-sm mx-auto">
+                <form onSubmit={handleLogin}>
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="email">Email</label>
+                      <input onChange={(e) => setEmail(e.target.value)} id="email" type="email" className="form-input w-full text-gray-800" placeholder="Email adresinizi giriniz" required />
+                    </div>
                   </div>
-                  <input onChange={(e) => setPassword(e.target.value)} id="password" type="password" className="form-input w-full text-gray-800" placeholder="Parolanızı giriniz" required />
+                  <div className="flex flex-wrap -mx-3 mb-4">
+                    <div className="w-full px-3">
+                      <div className="flex justify-between">
+                        <label className="block text-gray-800 text-sm font-medium mb-1" htmlFor="password">Parola</label>
+                        <Link href="/reset-password" className="text-sm font-medium text-blue-600 hover:underline">Parolamı Unuttum ?</Link>
+                      </div>
+                      <input onChange={(e) => setPassword(e.target.value)} id="password" type="password" className="form-input w-full text-gray-800" placeholder="Parolanızı giriniz" required />
+                    </div>
+                  </div>
+
+                  <div className="flex flex-wrap -mx-3 mt-6">
+                    <div className="w-full px-3">
+                      <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Giriş Yap</button>
+                    </div>
+                  </div>
+                </form>
+                <div className="flex items-center my-6">
+                  <div className="border-t border-gray-300 grow mr-3" aria-hidden="true"></div>
+                  <div className="text-gray-600 italic">Or</div>
+                  <div className="border-t border-gray-300 grow ml-3" aria-hidden="true"></div>
+                </div>
+                <div className="text-gray-600 text-center mt-6">
+                  Hesabınız Yok Mu? <Link href="/signup" className="text-blue-600 hover:underline transition duration-150 ease-in-out">Sign up</Link>
                 </div>
               </div>
 
-              <div className="flex flex-wrap -mx-3 mt-6">
-                <div className="w-full px-3">
-                  <button className="btn text-white bg-blue-600 hover:bg-blue-700 w-full">Giriş Yap</button>
-                </div>
-              </div>
-            </form>
-            <div className="flex items-center my-6">
-              <div className="border-t border-gray-300 grow mr-3" aria-hidden="true"></div>
-              <div className="text-gray-600 italic">Or</div>
-              <div className="border-t border-gray-300 grow ml-3" aria-hidden="true"></div>
-            </div>
-            <div className="text-gray-600 text-center mt-6">
-              Hesabınız Yok Mu? <Link href="/signup" className="text-blue-600 hover:underline transition duration-150 ease-in-out">Sign up</Link>
             </div>
           </div>
-
-        </div>
-      </div>
+      }
     </section>
   )
 }
--- feedback_project/client/app/dashboard/page.tsx
+++ feedback_project/client/app/dashboard/page.tsx
@@ -1,34 +1,67 @@
 'use client'
 import { Grid } from '@mantine/core'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+type Counts = {
+  Tebrik: number,
+  Öneri: number,
+  İstek: number,
+  Şikayet: number
+}
 
 export default function page() {
+  const [totalFeedback, setTotalFeedback] = useState(0)
+  const [counts, setCounts] = useState<Counts>()
+
+
+  useEffect(() => {
+    const getFeedbackCounts = async () => {
+      const res = await fetch("http://localhost:5000/api/v1/feedbacks/count", {
+        method: "GET",
+        credentials: "include"
+      })
+      const data = await res.json()
+      console.log(data);
+      if (res.ok) {
+        setTotalFeedback(data.total)
+        setCounts(data.counts)
+      }
+    }
+    getFeedbackCounts()
+  }, [])
+
   return (
     <div>
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="grid grid-cols-2 row-gap-8 md:grid-cols-4">
+      <div className="px-4 py-10 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-3">
+        <div className="text-center md:border-b pb-3">
+          <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl"> {totalFeedback} </h6>
+          <p className="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base">
+            Toplam Geri Bildirim
+          </p>
+        </div>
+        <div className="grid grid-cols-2 row-gap-8 md:grid-cols-4 mt-3">
           <div className="text-center md:border-r">
-            <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl">144K</h6>
+            <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl"> {counts?.Tebrik} </h6>
             <p className="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base">
-              Downloads
+              Tebrik
             </p>
           </div>
           <div className="text-center md:border-r">
-            <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl">12.9K</h6>
+            <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl"> {counts?.Öneri} </h6>
             <p className="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base">
-              Subscribers
+              Öneri
             </p>
           </div>
           <div className="text-center md:border-r">
-            <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl">48.3K</h6>
+            <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl"> {counts?.İstek} </h6>
             <p className="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base">
-              Users
+              İstek
             </p>
           </div>
           <div className="text-center">
-            <h6 className="text-4xl font-bold lg:text-5xl xl:text-6xl">24.5K</h6>
+            <h6 className="text-3xl font-bold lg:text-4xl xl:text-5xl"> {counts?.Şikayet} </h6>
             <p className="text-sm font-medium tracking-widest text-gray-800 uppercase lg:text-base">
-              Cookies
+              Şikayet
             </p>
           </div>
         </div>
