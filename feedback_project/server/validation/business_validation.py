28d405f234102b437483c917d12334ec7e6fb581
Tolga BAYRAK <bayraktolga28@gmail.com>
2023-05-17 22:19:21 +0300

    business validation

--- feedback_project/server/validation/business_validation.py
+++ feedback_project/server/validation/business_validation.py
@@ -1,6 +1,8 @@
-
-
-
 businessCreateSchema = {
-    
+    "type": "object",
+    "properties": {
+        "name": {"type": "string", "minLength": 3, "maxLength": 30},
+        "adddres": {"type": "string", "minLength": 10, "maxLength": 100},
+        "email": {"type": "string"}    
+    },
 }
